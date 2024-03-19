4454730e67b163053710f0daea72a0e305b4dad3
spasea <ithespv@gmail.com>
2024-03-20 00:31:38 +0200

    Update start script

--- start.sh
+++ start.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-PORT=6100 pm2 start dist/app.js --name facts_service --exp-backoff-restart-delay=200
+PORT=6100 pm2 start dist/index.js --name facts_service --exp-backoff-restart-delay=200
